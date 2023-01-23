f3ca13f1262920a6b2e20cde1e50cf7268d1ee83
vitoriamatos <vitoria.matos99@gmail.com>
2023-01-23 16:42:29 -0300

    implementin css hover effect in prouct list

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+$(document).ready(function () {
+
+
+    for (let i = 0; i <= 6; i++) {
+
+        $("#image" + i).hover(
+            function () {
+                $(this).attr("src", './images/tshirts/product' + i + '-2.png');
+            },
+            function () {
+                $(this).attr("src", './images/tshirts/product' + i + '.png');
+            }
+        );
+
+        touch = document.getElementById("#image" + i);
+
+        function touch(event) {
+            if (event.type == 'touchstart') {
+                $(this).attr("src", './images/tshirts/product' + i + '-2.png');
+            }
+            if (event.type == 'touchend') {
+                $(this).attr("src", './images/tshirts/product' + i + '.png');
+            }
+        }
+    }
+
+});
